dc8afed51a33ef07eb809c1c2a88bdc1d8897589
Alexei Pancratov <drivergear@gmail.com>
2021-06-03 09:02:59 -0400

    Added Smart Contracts support

---
+++ smart-contracts/scripts/sample-script.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const StandardErc20 = await hre.ethers.getContractFactory("StandardERC20");
+  const standardErc20 = await StandardErc20.deploy("GBC Token", "GBC", 100000000000000000000);
+
+  await standardErc20.deployed();
+
+  console.log("StandardErc20 deployed to:", standardErc20.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
