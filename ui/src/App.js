02cea5d17091b4a5e68576d882f6974d080620ed
dhruvampatel <dhruvam.sp1@gmail.com>
2021-06-04 14:32:25 -0400

    made changes to ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,46 +2,47 @@ import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 import "./App.css";
 import abi from './contractAbis/erc20Abi.json';
-import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import BuyGet from "./components/buyGet/buyGet";
 import BuyHistory from "./components/buyHistory/buyHistory";
 import Header from './components/header/Header';
 import Compensate from './components/compensate/Compensate';
 
 function App() {
-  let erc20Instance;
+  const [instance, setInstance] = useState();
 
   useEffect(() => {
     const createInstance = () => {
       let web3;
       if (window.ethereum) {
         web3 = new Web3(window.ethereum);
-        erc20Instance = new web3.eth.Contract( abi, "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+        let erc20Instance = new web3.eth.Contract(abi, "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+        setInstance(erc20Instance);
       }
     }
 
     createInstance();
-  });
+  }, []);
     
   return (
     <BrowserRouter>
       <div>
           <Header />
-          <Switch>
+          {!instance ? null : <Switch>
             <Route
               path="/"
-              component={() => <BuyGet instance={erc20Instance}/>}
+              component={() => <BuyGet instance={instance}/>}
               exact
             />
             <Route
               path="/history"
-              component={() => <BuyHistory instance={erc20Instance}/>}
+              component={() => <BuyHistory instance={instance}/>}
             />
             <Route
               path="/compensate"
-              component={() => <Compensate instance={erc20Instance}/>}
+              component={() => <Compensate instance={instance}/>}
             />
-          </Switch>
+          </Switch>}
       </div>
     </BrowserRouter>
   );
